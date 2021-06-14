8c157aab1e7c2a61f56a949a9465f229fd4b3a40
Mitesh <miteshsonekar@gmail.com>
2021-06-14 08:11:23 +0530

    added router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        this is home page
+      </Route>
+
+      <Route exact path="/starred">
+        this is a starred page
+      </Route>
+
+      <Route path="/another">this is anoher</Route>
+    </Switch>
+  );
 }
 
 export default App;
